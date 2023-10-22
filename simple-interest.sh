9e6cb8e2ae899a5ba879974d1ce6d64c462b07f9
mariampinto <142305311+mariampinto@users.noreply.github.com>
2023-10-22 13:54:43 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
 # Do not use this in production. Sample purpose only.
 
-# Author: Upkar Lidder (IBM)
+# Author: mariampinto
 # Additional Authors:
-# MichaelObasa
+ 
 
 # Input:
 # p, principal amount
